9215f74dd94eb55f147368240f357e162ce3fcbd
Ivan Khanevich <mail@iamkhan.tech>
2022-12-12 22:29:52 +0400

    chore: add new build system, CI/CD and update deps

---
+++ README.md
@@ -0,0 +1,7 @@
+# @iamkhan21/fxform [![NPM version](https://img.shields.io/npm/v/@iamkhan21/fxform?color=a1b858&label=)](https://www.npmjs.com/package/@iamkhan21/fxform)
+
+Reactive forms based on effector and zod
+
+
+## License
+[MIT](./LICENSE) License © 2022 [Ivan Khanevich](https://github.com/iamkhan21)
--- src/index.ts
+++ src/index.ts
@@ -4,82 +4,82 @@ import { debounce } from "patronum/debounce";
 import type { Field, Fields, FieldsSources, Form } from "./types";
 
 function createField(initialValue: string, validator: ZodType): Field {
-  const filed = createDomain();
-  const value$ = filed.createStore(initialValue);
+	const filed = createDomain();
+	const value$ = filed.createStore(initialValue);
 
-  const error$ = filed.createStore<string>("");
-  const isTouched$ = filed.createStore(false);
-  const isValid$ = filed.createStore(validator.safeParse(initialValue).success);
+	const error$ = filed.createStore<string>("");
+	const isTouched$ = filed.createStore(false);
+	const isValid$ = filed.createStore(validator.safeParse(initialValue).success);
 
-  const set = filed.createEvent<string>();
-  const reset = filed.createEvent();
+	const set = filed.createEvent<string>();
+	const reset = filed.createEvent();
 
-  const validateFx = filed.createEffect<
-    string,
-    SafeParseReturnType<string, string>
-  >((value) => validator.safeParse(value));
+	const validateFx = filed.createEffect<
+		string,
+		SafeParseReturnType<string, string>
+	>((value) => validator.safeParse(value));
 
-  debounce({
-    source: set,
-    timeout: 350,
-    target: validateFx,
-  });
+	debounce({
+		source: set,
+		timeout: 350,
+		target: validateFx,
+	});
 
-  value$.reset(reset).on(set, (_, value) => value);
-  error$
-    .reset(reset)
-    .on(validateFx.doneData, (_, result) =>
-      result.success ? "" : result.error.issues[0].message
-    );
-  isValid$
-    .reset(reset)
-    .on(set, () => false)
-    .on(validateFx.doneData, (_, result) => result.success);
-  isTouched$.reset(reset).on(set, () => true);
+	value$.reset(reset).on(set, (_, value) => value);
+	error$
+		.reset(reset)
+		.on(validateFx.doneData, (_, result) =>
+			result.success ? "" : result.error.issues[0].message,
+		);
+	isValid$
+		.reset(reset)
+		.on(set, () => false)
+		.on(validateFx.doneData, (_, result) => result.success);
+	isTouched$.reset(reset).on(set, () => true);
 
-  return { value$, error$, isTouched$, isValid$, set, reset };
+	return { value$, error$, isTouched$, isValid$, set, reset };
 }
 
 export function createForm<T extends Record<string, [string, ZodType]>>(
-  formData: T
+	formData: T,
 ): Form<T> {
-  const fields = {} as Fields<T>;
-  const fieldSources = {} as FieldsSources<T>;
+	const fields = {} as Fields<T>;
+	const fieldSources = {} as FieldsSources<T>;
 
-  for (const [key, value] of Object.entries(formData)) {
-    const field = createField(...value);
-    fieldSources[key as keyof T] = field.value$;
-    fields[key as keyof T] = field;
-  }
+	for (const [key, value] of Object.entries(formData)) {
+		const field = createField(...value);
+		fieldSources[key as keyof T] = field.value$;
+		fields[key as keyof T] = field;
+	}
 
-  const form = createDomain();
-  const reset = form.createEvent<void>();
-  const submit = form.createEvent<void>();
-  const submitted = form.createEvent<Record<keyof T, string>>();
+	const form = createDomain();
+	const reset = form.createEvent<void>();
+	const submit = form.createEvent<void>();
+	const submitted = form.createEvent<Record<keyof T, string>>();
 
-  // @ts-ignore
-  sample({
-    clock: submit,
-    source: fieldSources,
-    fn: (formValues: Record<keyof T, string>) => formValues,
-    target: submitted,
-  });
+	// @ts-ignore
+	sample({
+		clock: submit,
+		source: fieldSources,
+		fn: (formValues: Record<keyof T, string>) => formValues,
+		target: submitted,
+	});
 
-  forward({
-    from: reset,
-    to: Object.values(fields).map((field) => field.reset),
-  });
+	forward({
+		from: reset,
+		to: Object.values(fields).map((field) => field.reset),
+	});
 
-  const isValid$ = combine(
-    Object.values(fields).map((field) => field.isValid$),
-    (states) => states.every(Boolean)
-  );
+	const isValid$ = combine(
+		Object.values(fields).map((field) => field.isValid$),
+		(states) => states.every(Boolean),
+	);
 
-  return {
-    fields,
-    isValid$,
-    reset,
-    submit,
-    submitted,
-  };
+	return {
+		fields,
+		isValid$,
+		reset,
+		submit,
+		submitted,
+	};
 }
